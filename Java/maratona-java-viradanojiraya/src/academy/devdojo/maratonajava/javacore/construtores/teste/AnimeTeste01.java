6346cc5e126a4ad9cabbfaa7ff632fe04f3cb464
juanpb1 <jpprog1@gmail.com>
2022-03-15 13:09:53 -0300

    Construtores

---
+++ Java/maratona-java-viradanojiraya/src/academy/devdojo/maratonajava/javacore/construtores/teste/AnimeTeste01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.construtores.teste;
+
+import academy.devdojo.maratonajava.javacore.construtores.dominion.*;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Haikyuu","TV", 12, "Esporte", "Production I.G.");
+
+        anime.imprime();
+    }
+}
